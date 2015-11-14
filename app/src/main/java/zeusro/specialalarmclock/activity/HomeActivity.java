7d25b42bccc808764dd405e7991865398daa02c2
zeusro <692486802@QQ.COM>
2015-11-14 21:04:23 +0800

    开始做首页

--- app/src/main/java/zeusro/specialalarmclock/activity/HomeActivity.java
+++ app/src/main/java/zeusro/specialalarmclock/activity/HomeActivity.java
@@ -1,20 +1,114 @@
 package zeusro.specialalarmclock.activity;
 
-import zeusro.specialalarmclock.R;
-
-import android.support.v7.app.AppCompatActivity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import java.lang.reflect.Field;
+
+import zeusro.specialalarmclock.R;
+
+public class HomeActivity extends AppCompatActivity implements android.view.View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if (menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception ex) {
+            // Ignore
+        }
+
         setContentView(R.layout.activity_home);
-        Toast toast = Toast.makeText(getApplicationContext(), R.string.Thank,Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, R.string.Thank, Toast.LENGTH_SHORT);
         //显示toast信息
         toast.show();
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String url = null;
+        Intent intent = null;
+        switch (item.getItemId()) {
+            case R.id.menu_item_new:
+                Intent newAlarmIntent = new Intent(this, AlarmPreferencesActivity.class);
+                startActivity(newAlarmIntent);
+                break;
+            case R.id.menu_item_rate:
+                url = "market://details?id=" + getPackageName();
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                try {
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(this, "Couldn't launch the market", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.menu_item_website:
+                url = "http://www.neilson.co.za";
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                try {
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(this, "Couldn't launch the website", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.menu_item_report:
+
+                url = "https://github.com/SheldonNeilson/Android-Alarm-Clock/issues";
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                try {
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(this, "Couldn't launch the bug reporting website", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void callMathAlarmScheduleService() {
+        Intent mathAlarmServiceIntent = new Intent(this, AlarmServiceBroadcastReciever.class);
+        sendBroadcast(mathAlarmServiceIntent, null);
+    }
+
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
+    }
+
+
 }
