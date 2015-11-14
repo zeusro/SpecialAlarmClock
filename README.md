fc5316f7020692ad5a6dc401b72ffda2d6294c09
zeusro <692486802@QQ.COM>
2015-11-14 19:51:07 +0800

    替换默认图标

--- README.md
+++ README.md
@@ -7,19 +7,28 @@
 点击XX后提前停止闹钟,并且记录停止时间
 
 ## 开发日程:
-移植界面.
+2015-11-14~2015-11-21 移植界面.
 
-移植功能
+一星期：移植功能
 
-定制开发
+日后再说：
+定制开发，实现例外，记录起床时间，同步起床记录
 
 
 
 ## todo:
+
+
+## improve：
+
 假期自动跳过;
+
+
 选取铃声作为闹钟声音
 
+## 参考项目：
 
+http://stackoverflow.com/questions/7594637/alarm-clock-application-source-code
 
 https://github.com/SheldonNeilson/Android-Alarm-Clock
 
--- app/src/main/java/zeusro/specialalarmclock/activity/HomeActivity.java
+++ app/src/main/java/zeusro/specialalarmclock/activity/HomeActivity.java
@@ -1,8 +1,10 @@
 package zeusro.specialalarmclock.activity;
 
-import  zeusro.specialalarmclock.R;
+import zeusro.specialalarmclock.R;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toast toast = Toast.makeText(getApplicationContext(), R.string.Thank,Toast.LENGTH_SHORT);
+        //显示toast信息
+        toast.show();
+
     }
 }
