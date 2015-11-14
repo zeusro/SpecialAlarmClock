92f2ab3de1c684582b8a22b88f41ffd25e76a5ab
zeusro <692486802@QQ.COM>
2015-11-14 19:14:53 +0800

    增加开发日程

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # SpecialAlarmClock
+
 自制的Android闹钟
+
+简单设置周期.
+
+点击XX后提前停止闹钟,并且记录停止时间
+
+## 开发日程:
+移植界面.
+
+移植功能
+
+定制开发
+
+
+
+## todo:
+假期自动跳过;
+选取铃声作为闹钟声音
+
+
+
+https://github.com/SheldonNeilson/Android-Alarm-Clock
+
+https://github.com/yuriykulikov/AlarmClock
+
+https://github.com/bedditor/ohtu-Alarmclock
+
+https://github.com/isaacloud/android-alarm-clock
---
+++ app/src/main/java/zeusro/specialalarmclock/activity/HomeActivity.java
@@ -0,0 +1,14 @@
+package zeusro.specialalarmclock.activity;
+
+import  zeusro.specialalarmclock.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
