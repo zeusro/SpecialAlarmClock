0c6df88e101c94b165c2c053677364ceef9a4cfe
zeusro <692486802@QQ.COM>
2015-11-16 15:56:40 +0800

    完成首页界面初步设计

--- app/src/main/java/zeusro/specialalarmclock/activity/HomeActivity.java
+++ app/src/main/java/zeusro/specialalarmclock/activity/HomeActivity.java
@@ -1,13 +1,7 @@
 package zeusro.specialalarmclock.activity;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.widget.Toast;
@@ -39,64 +33,6 @@ public class HomeActivity extends AppCompatActivity implements android.view.View
         //显示toast信息
         toast.show();
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.mainmenu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        String url = null;
-        Intent intent = null;
-        switch (item.getItemId()) {
-            case R.id.menu_item_new:
-                Intent newAlarmIntent = new Intent(this, AlarmPreferencesActivity.class);
-                startActivity(newAlarmIntent);
-                break;
-            case R.id.menu_item_rate:
-                url = "market://details?id=" + getPackageName();
-                intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(url));
-                try {
-                    startActivity(intent);
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(this, "Couldn't launch the market", Toast.LENGTH_LONG).show();
-                }
-                break;
-            case R.id.menu_item_website:
-                url = "http://www.neilson.co.za";
-                intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(url));
-                try {
-                    startActivity(intent);
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(this, "Couldn't launch the website", Toast.LENGTH_LONG).show();
-                }
-                break;
-            case R.id.menu_item_report:
-
-                url = "https://github.com/SheldonNeilson/Android-Alarm-Clock/issues";
-                intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(url));
-                try {
-                    startActivity(intent);
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(this, "Couldn't launch the bug reporting website", Toast.LENGTH_LONG).show();
-                }
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    protected void callMathAlarmScheduleService() {
-        Intent mathAlarmServiceIntent = new Intent(this, AlarmServiceBroadcastReciever.class);
-        sendBroadcast(mathAlarmServiceIntent, null);
     }
 
 
